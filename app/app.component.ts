d0a855ec65d214c159877d1cfbc3affd486bcc74
Astrid Sutton <astrid.sutton@wgu.edu>
2016-11-18 14:59:24 -0700

    working through tutorial

--- app/app.component.ts
+++ app/app.component.ts
@@ -22,17 +22,21 @@ const HEROES: Hero[] = [
     selector: 'my-app',
     template: `
         <h1>{{title}}</h1>
-        <h2>{{hero.name}} details!</h2>
-        <div>
-            <label>id: </label>{{hero.id}}
+        
+        <div *ngIf="selectedHero">
+            <h2>{{selectedHero.name}} details!</h2>
+            <div>
+                <label>id: </label>{{selectedHero.id}}
+            </div>
+            <div>
+                <label>name: </label>
+                <input [(ngModel)]="selectedHero.name" placeholder="name">
+            </div>
         </div>
-        <div>
-            <label>name: </label>
-            <input [(ngModel)]="hero.name" placeholder="name">
-        </div>
+
         <h2>My Heroes</h2>
         <ul class="heroes">
-            <li *ngFor="let hero of heroes" (click)="onSelect(hero)">
+            <li *ngFor="let hero of heroes" (click)="onSelect(hero)" [class.selected]="hero === selectedHero">
                 <!-- each hero goes here -->
                 <span class="badge">{{hero.id}}</span> {{hero.name}}
             </li>
@@ -91,9 +95,13 @@ const HEROES: Hero[] = [
 
 export class AppComponent {
     title = 'Tour of Heroes';
-    hero: Hero = {
-        id: 1,
-        name: 'Windstorm'
-    };
+    // hero: Hero = {
+    //     id: 1,
+    //     name: 'Windstorm'
+    // };
     heroes = HEROES;
+    selectedHero: Hero;
+    onSelect(hero: Hero): void {
+        this.selectedHero = hero;
+    }
 }
