1cdf92d5a4d5ce7df04db52f7f2b1b3248f307be
Astrid Sutton <astrid.sutton@wgu.edu>
2016-11-21 10:33:13 -0700

    end 'return mocked heros'

--- app/app.component.js
+++ app/app.component.js
@@ -15,22 +15,21 @@ var Hero = (function () {
     return Hero;
 }());
 exports.Hero = Hero;
-var HEROES = [
-    { id: 11, name: 'Mr. Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
-];
+// const HEROES: Hero[] = [
+//     { id: 11, name: 'Mr. Nice' },
+//     { id: 12, name: 'Narco' },
+//     { id: 13, name: 'Bombasto' },
+//     { id: 14, name: 'Celeritas' },
+//     { id: 15, name: 'Magneta' },
+//     { id: 16, name: 'RubberMan' },
+//     { id: 17, name: 'Dynama' },
+//     { id: 18, name: 'Dr IQ' },
+//     { id: 19, name: 'Magma' },
+//     { id: 20, name: 'Tornado' }
+// ];
 var AppComponent = (function () {
     function AppComponent() {
         this.title = 'Tour of Heroes';
-        this.heroes = HEROES;
     }
     AppComponent.prototype.onSelect = function (hero) {
         this.selectedHero = hero;
--- app/app.component.ts
+++ app/app.component.ts
@@ -5,18 +5,18 @@ export class Hero {
     name: string;
 }
 
-const HEROES: Hero[] = [
-    { id: 11, name: 'Mr. Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
-];
+// const HEROES: Hero[] = [
+//     { id: 11, name: 'Mr. Nice' },
+//     { id: 12, name: 'Narco' },
+//     { id: 13, name: 'Bombasto' },
+//     { id: 14, name: 'Celeritas' },
+//     { id: 15, name: 'Magneta' },
+//     { id: 16, name: 'RubberMan' },
+//     { id: 17, name: 'Dynama' },
+//     { id: 18, name: 'Dr IQ' },
+//     { id: 19, name: 'Magma' },
+//     { id: 20, name: 'Tornado' }
+// ];
 
 @Component({
     selector: 'my-app',
@@ -86,7 +86,8 @@ const HEROES: Hero[] = [
 
 export class AppComponent {
     title = 'Tour of Heroes';
-    heroes = HEROES;
+    // heroes = HEROES;
+    heroes: Hero[];
     selectedHero: Hero;
 
     onSelect(hero: Hero): void {
